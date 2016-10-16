38122cfed76aa24b965b74932ff71b6d0f672b1d
Christian Weber <webdog@github.com>
2016-10-16 16:35:54 -0500

    Moved all outputs to lists of dicts, and first run of outputting to csv

--- datagen/gh-reports.py
+++ datagen/gh-reports.py
@@ -40,10 +40,10 @@ gh = authenticate()
 def get_org():
 	for orgs in gh.iter_orgs():
 		# We only allow an org at a time for restrict rate-limiting or performance issues
-		org = [orgs.id for orgs in gh.iter_orgs() if orgs.name == args.org ]
+		org = [orgs for orgs in gh.iter_orgs() if orgs.login == args.org ]
 	return org
 
-def get_repos(org_search=False, user_search=True):
+def get_repos(org_search=True, user_search=False):
 	if org_search:
 		for orgs in get_org():
 			repos = [repos for repos in orgs.iter_repos() if repos.name == args.repo]
@@ -51,15 +51,17 @@ def get_repos(org_search=False, user_search=True):
 	elif user_search:
 		repo = [r.refresh() for r in gh.iter_user_repos(user) if r.name == args.repo]
 		return repo
+	elif org_search and user_search:
+		return "Cannot return both user and org repos"
 	else:
 		repo = [r.refresh() for r in gh.iter_user_repos('angular') if r.name == 'angular']
 		return repo
 
 
-def get_prs(limit=100):
+def get_prs(limit=100, state='all'):
 	pulls = []
 	for repo in get_repos():
-		pr = repo.iter_pulls(state='all', number=limit)
+		pr = repo.iter_pulls(state=state, number=limit)
 		pr.refresh()
 		for prs in pr:
 			pulls.append(prs)
@@ -84,21 +86,64 @@ def report(rtype):
 		return "Expected an integer value. Exiting"
 	elif rtype == 1:
 		repo = get_repos()
-		report = Report(repo).user_languages()
+		report = Report(repo=repo).user_languages()
 		return report
 	elif rtype == 2:
-		report = Report.bla()
+		pulls = get_prs()
+		report = Report(pulls=pulls).pr_ratios()
 		return report
-
-
+	elif rtype == 3:
+		pulls = get_prs(state='closed')
+		report = Report(pulls).pr_ratios()
+		return report
+	elif rtype == 4:
+		pulls = get_prs()
+		report = Report(pulls=pulls).contribution_count()
+		return report
+	elif rtype == 5:
+		pulls = get_prs()
+		report = Report(pulls=pulls).repo_maintenance()
+		return report
+	elif rtype == 6:
+		repo = get_repos()
+		report = Report(repo=repo).open_issues()
+		return report
+	elif rtype == 7:
+		repo = get_repos()
+		report = Report(repo=repo).issue_comments()
+		return report
+	elif rtype == 8:
+		#This is a heavy computation. Limiting to 10 PRs, as we're analyzing each sentence
+		#in each comment in a single PR
+		pulls = get_prs(limit=10)
+		report = Report(pulls=pulls).pr_sentiment()
+		print(out2csv(report))
+		return report
 	else:
 		return True
 
+def out2csv(lt, fname="tmp.csv"):
+	if not isinstance(lt, list):
+		return "Expected a list"
+	else:
+		#This function is generally intaking lists that contain multiple dictionaries.
+		#We only want the headers to consist of a single list comprised of the keys from these dicts
+		headers = set()
+		for d in lt:
+			headers.update(d.keys())
+
+		print(headers)
+		with open(fname, "w+") as f:
+			dw = csv.DictWriter(f, delimiter='|', fieldnames=headers)
+			dw.writerow(dict((fn, fn) for fn in headers))
+			for d in lt:
+				dw.writerow(d)
+		return True
 def main():
 	rt = args.report_type
 	keys = [keys for keys, values in valid_reports().items()]
 	if rt not in keys:
-		return "You did not select a valid report"
+		print("Please choose a valid selection:\n")
 	else:
 		return report(rt)
 
@@ -108,5 +153,4 @@ def main():
 
 
 if __name__ == "__main__":
-	print("running main")
 	main()
--- datagen/reports.py
+++ datagen/reports.py
@@ -1,68 +1,215 @@
 #!/usr/bin/env python3
+from textblob import TextBlob
+from textblob.sentiments import NaiveBayesAnalyzer
 
 class Report(object):
-	def __init__(self, repo=None, org=None):
+	def __init__(self, repo=None, org=None, pulls=None):
 		self.repo = repo
 		self.org = org
+		self.pulls = pulls
 
 
 	def user_languages(self):
-		commit_list = []
-		sha_list = []
-		committer_and_file_type = []
+		clist = []
 		for r in self.repo:
 			commits = r.iter_commits(number=11)
-			print(commits)
 			for c in commits:
-				max_shas = 0
-				while max_shas < 10:
-					sha = c.sha
-					sha_list.append(sha)
-					max_shas = max_shas + 1
-
-
-		for sha in sha_list:
-			commit_list.append(r.commit(sha).to_json())
-
-		for cj in commit_list:
-			try:
-				files = cj['files'][0]['filename']
-				committer = cj['commit']['author']['name']
-				sha = cj['sha'][0:6]
-				continue
-			except TypeError:
-				continue
-			except IndexError:
-				continue
-
-			finally:
-				if "/" in files:
-					files = files.split("/")[-1]
-					extension = files.split(".")[-1]
-					committer_and_file_type.append([sha, committer, extension])
-				elif "." in files:
-					files = files.split(".")[-1]
-					committer_and_file_type.append([sha, committer, files])
+				cdict = {}
+				cfiles = str(r.commit(c.sha).files[0]['filename'])
+				print(r.commit(c.sha).files)
+				committer = str(r.commit(c.sha).author)
+				if "/" in cfiles:
+					files = cfiles.split("/")[-1].split(".")[-1]
+					cdict['sha'] = c.sha
+					cdict['filetype'] = files
+					cdict['author'] = committer
 				else:
-					pass
+					files = cfiles.split(".")[-1]
+					cdict['sha'] = c.sha
+					cdict['filetype'] = files
+					cdict['author'] = committer
+				clist.append(cdict)
 
-		print(committer_and_file_type)
-		return committer_and_file_type
+		print(clist)
+		return clist
 
-	def pr_ratios(self, state='Closed'):
-		pass
+	def pr_ratios(self):
+		plmrg = "pull_merged"
+		plnmrg = "pull_not_merged"
+		pltot = "pull_total"
+		pdict = {
+				plmrg: 0,
+				plnmrg: 0,
+				pltot: 0
+			}
+		for pr in self.pulls:
+			if pr.is_merged():
+				pdict[plmrg] += 1
+			else:
+				pdict[plnmrg] += 1
+		pdict[pltot] = pdict[plmrg] + pdict[plnmrg]
+
+		print(pdict)
+		return pdict
 
 	def contribution_count(self):
-		pass
+		plist = []
+		pos = "net_positive"
+		neg = "net_negative"
+		neq = "net_equal"
+		number = "number"
+		adds = "additions"
+		dels = "deletions"
+		for pr in self.pulls:
+			pr = pr.refresh()
+
+			pdict = {
+				pos : 0,
+				neg : 0,
+				neq : 0,
+				adds : 0,
+				dels : 0,
+				number: ""
+
+			}
+			#pr = self.pulls.refresh()
+
+			if pr.additions is None:
+				pr.additions = 0
+
+			if pr.deletions is None:
+				pr.deletions = 0
+
+			pdict[number] = pr.number
+			pdict[adds] = pr.additions
+			pdict[dels] = pr.deletions
+			total = pr.additions - pr.deletions
+			if total < 0:
+				pdict[neg] = total
+			elif total > 0:
+				pdict[pos] = total
+			else:
+				pdict[neq] = total
+			plist.append(pdict)
+
+		return(plist)
 
 	def repo_maintenance(self):
-		pass
+		rcontrib = "repo_contrib"
+		rmaintain = "repo_maintain"
+		number = "number"
+		plist = []
+
+		for pr in self.pulls:
+			pr = pr.refresh()
+			pdict = {
+			rcontrib : 0,
+			rmaintain : 0,
+			number : "number"
+			}
+
+			pr = pr.refresh()
+			files = pr.iter_files()
+			for PullFile in files:
+				fname = PullFile.filename.split(".")[1]
+				pdict[number] = pr.number
+				if fname == "md":
+					pdict[rcontrib] += 1
+				elif fname == "txt":
+					pdict[rmaintain]+= 1
+				else:
+					pdict[rcontrib] += 1
+				plist.append(pdict)
+		return(plist)
 
 	def open_issues(self):
-		pass
+		alist = []
+		assigned = "assigned"
+		nassigned = "not_assigned"
+		iname = "assigned_name"
+		number = "number"
+		acount = {
+			assigned : 0,
+			nassigned : 0
+		}
+		for r in self.repo:
 
-	def issue_comments(self, state='Closed'):
-		pass
+			issues = r.iter_issues(number=100, state='open')
+			for i in issues:
+				i = i.refresh()
+				adict = {}
+
+				assignee = str(i.assignee)
+
+				adict[number] = i.number
+				if assignee != "None":
+					adict[iname] = assignee
+					acount[assigned] += 1
+				elif assignee == "None":
+					acount[nassigned] += 1
+				else:
+					acount[assigned] += 1
+				alist.append(adict)
+
+		#Return a tuple of the count of assigned vs unassigned, and the specific issue information
+		rtup = (alist, acount)
+		return rtup
+
+
+	def issue_comments(self, state='closed'):
+		clist = []
+		for r in self.repo:
+			issues = r.iter_issues(number=100, state=state)
+			rname = "repo_name"
+			number = "issue_number"
+			ncomments = "number_of_comments"
+
+			for i in issues:
+				cdict = {}
+				i = i.refresh()
+				cdict[rname] = r.name
+				cdict[number] = i.number
+				cdict[ncomments] = i.comments
+				clist.append(cdict)
+		return clist
+
 
 	def pr_sentiment(self):
-		pass
+		#sentiment list
+		slist = []
+		comlist = []
+		for pr in self.pulls:
+			pr = pr.refresh()
+			number = "number"
+			body = "body"
+			author = "comment_author"
+			comment = pr.iter_comments()
+			comments = comment.refresh()
+			for c in comments:
+				comdict = {}
+				comdict[number] = pr.number
+				comdict[body] = c.body
+				user = c.user.refresh()
+				comdict[author] = user.name
+				comlist.append(comdict)
+
+		for i in comlist:
+			sdict = {}
+			comment = i['body']
+			number = i['number']
+			author = i['comment_author']
+			classifier = "classified_as"
+			overall = "overall_positivity"
+			blob = TextBlob(comment, analyzer=NaiveBayesAnalyzer())
+			for sentences in blob.sentences:
+				sent = sentences.sentiment
+				sdict['comment'] = comment
+				sdict['number'] = number
+				sdict['author'] = author
+				sdict[classifier] = sent[0]
+				sdict[overall] = sent[1]
+			slist.append(sdict)
+
+		print(slist)
+		return slist
+
